0c28e316500d01a17c3759f7b05b48b27be99bec
Milos <dzavid.bestija@gmail.com>
2021-04-07 16:20:44 +0200

    Added Week15 teaching material with database file and win form project

---
+++ Week15/ConnectingToDBapp/EntityPractice/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace EntityPractice
+{
+    partial class SchoolForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectStudent = new System.Windows.Forms.Button();
+            this.btnSelectAddress = new System.Windows.Forms.Button();
+            this.btnSelectCourse = new System.Windows.Forms.Button();
+            this.btnInsertCourse = new System.Windows.Forms.Button();
+            this.btnInsertAddress = new System.Windows.Forms.Button();
+            this.btnInsertStudent = new System.Windows.Forms.Button();
+            this.btnUpdateCourse = new System.Windows.Forms.Button();
+            this.btnUpdateAddress = new System.Windows.Forms.Button();
+            this.btnUpdateStudent = new System.Windows.Forms.Button();
+            this.btnDeleteCourse = new System.Windows.Forms.Button();
+            this.btnDeleteAddress = new System.Windows.Forms.Button();
+            this.btnDeleteStudent = new System.Windows.Forms.Button();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtCourseName = new System.Windows.Forms.TextBox();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblStudentName = new System.Windows.Forms.Label();
+            this.lblCourseName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSelectStudent
+            // 
+            this.btnSelectStudent.Location = new System.Drawing.Point(180, 87);
+            this.btnSelectStudent.Name = "btnSelectStudent";
+            this.btnSelectStudent.Size = new System.Drawing.Size(105, 23);
+            this.btnSelectStudent.TabIndex = 0;
+            this.btnSelectStudent.Text = "SelectStudent";
+            this.btnSelectStudent.UseVisualStyleBackColor = true;
+            this.btnSelectStudent.Click += new System.EventHandler(this.btnSelectStudent_Click);
+            // 
+            // btnSelectAddress
+            // 
+            this.btnSelectAddress.Location = new System.Drawing.Point(336, 87);
+            this.btnSelectAddress.Name = "btnSelectAddress";
+            this.btnSelectAddress.Size = new System.Drawing.Size(105, 23);
+            this.btnSelectAddress.TabIndex = 1;
+            this.btnSelectAddress.Text = "SelectAddress";
+            this.btnSelectAddress.UseVisualStyleBackColor = true;
+            // 
+            // btnSelectCourse
+            // 
+            this.btnSelectCourse.Location = new System.Drawing.Point(491, 87);
+            this.btnSelectCourse.Name = "btnSelectCourse";
+            this.btnSelectCourse.Size = new System.Drawing.Size(105, 23);
+            this.btnSelectCourse.TabIndex = 2;
+            this.btnSelectCourse.Text = "SelectCourse";
+            this.btnSelectCourse.UseVisualStyleBackColor = true;
+            // 
+            // btnInsertCourse
+            // 
+            this.btnInsertCourse.Location = new System.Drawing.Point(491, 116);
+            this.btnInsertCourse.Name = "btnInsertCourse";
+            this.btnInsertCourse.Size = new System.Drawing.Size(105, 23);
+            this.btnInsertCourse.TabIndex = 5;
+            this.btnInsertCourse.Text = "InsertCourse";
+            this.btnInsertCourse.UseVisualStyleBackColor = true;
+            // 
+            // btnInsertAddress
+            // 
+            this.btnInsertAddress.Location = new System.Drawing.Point(336, 116);
+            this.btnInsertAddress.Name = "btnInsertAddress";
+            this.btnInsertAddress.Size = new System.Drawing.Size(105, 23);
+            this.btnInsertAddress.TabIndex = 4;
+            this.btnInsertAddress.Text = "InsertAddress";
+            this.btnInsertAddress.UseVisualStyleBackColor = true;
+            // 
+            // btnInsertStudent
+            // 
+            this.btnInsertStudent.Location = new System.Drawing.Point(180, 116);
+            this.btnInsertStudent.Name = "btnInsertStudent";
+            this.btnInsertStudent.Size = new System.Drawing.Size(105, 23);
+            this.btnInsertStudent.TabIndex = 3;
+            this.btnInsertStudent.Text = "InsertStudent";
+            this.btnInsertStudent.UseVisualStyleBackColor = true;
+            this.btnInsertStudent.Click += new System.EventHandler(this.btnInsertStudent_Click);
+            // 
+            // btnUpdateCourse
+            // 
+            this.btnUpdateCourse.Location = new System.Drawing.Point(491, 145);
+            this.btnUpdateCourse.Name = "btnUpdateCourse";
+            this.btnUpdateCourse.Size = new System.Drawing.Size(105, 23);
+            this.btnUpdateCourse.TabIndex = 8;
+            this.btnUpdateCourse.Text = "UpdateCourse";
+            this.btnUpdateCourse.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdateAddress
+            // 
+            this.btnUpdateAddress.Location = new System.Drawing.Point(336, 145);
+            this.btnUpdateAddress.Name = "btnUpdateAddress";
+            this.btnUpdateAddress.Size = new System.Drawing.Size(105, 23);
+            this.btnUpdateAddress.TabIndex = 7;
+            this.btnUpdateAddress.Text = "UpdateAddress";
+            this.btnUpdateAddress.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdateStudent
+            // 
+            this.btnUpdateStudent.Location = new System.Drawing.Point(180, 145);
+            this.btnUpdateStudent.Name = "btnUpdateStudent";
+            this.btnUpdateStudent.Size = new System.Drawing.Size(105, 23);
+            this.btnUpdateStudent.TabIndex = 6;
+            this.btnUpdateStudent.Text = "UpdateStudent";
+            this.btnUpdateStudent.UseVisualStyleBackColor = true;
+            this.btnUpdateStudent.Click += new System.EventHandler(this.btnUpdateStudent_Click);
+            // 
+            // btnDeleteCourse
+            // 
+            this.btnDeleteCourse.Location = new System.Drawing.Point(491, 174);
+            this.btnDeleteCourse.Name = "btnDeleteCourse";
+            this.btnDeleteCourse.Size = new System.Drawing.Size(105, 23);
+            this.btnDeleteCourse.TabIndex = 11;
+            this.btnDeleteCourse.Text = "DeleteCourse";
+            this.btnDeleteCourse.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteAddress
+            // 
+            this.btnDeleteAddress.Location = new System.Drawing.Point(336, 174);
+            this.btnDeleteAddress.Name = "btnDeleteAddress";
+            this.btnDeleteAddress.Size = new System.Drawing.Size(105, 23);
+            this.btnDeleteAddress.TabIndex = 10;
+            this.btnDeleteAddress.Text = "DeleteAddress";
+            this.btnDeleteAddress.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteStudent
+            // 
+            this.btnDeleteStudent.Location = new System.Drawing.Point(180, 174);
+            this.btnDeleteStudent.Name = "btnDeleteStudent";
+            this.btnDeleteStudent.Size = new System.Drawing.Size(105, 23);
+            this.btnDeleteStudent.TabIndex = 9;
+            this.btnDeleteStudent.Text = "DeleteStudent";
+            this.btnDeleteStudent.UseVisualStyleBackColor = true;
+            this.btnDeleteStudent.Click += new System.EventHandler(this.btnDeleteStudent_Click);
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(336, 61);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(105, 20);
+            this.txtCity.TabIndex = 13;
+            // 
+            // txtCourseName
+            // 
+            this.txtCourseName.Location = new System.Drawing.Point(491, 61);
+            this.txtCourseName.Name = "txtCourseName";
+            this.txtCourseName.Size = new System.Drawing.Size(105, 20);
+            this.txtCourseName.TabIndex = 14;
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.Location = new System.Drawing.Point(180, 61);
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(105, 20);
+            this.txtStudentName.TabIndex = 15;
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(333, 45);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(24, 13);
+            this.lblCity.TabIndex = 16;
+            this.lblCity.Text = "City";
+            // 
+            // lblStudentName
+            // 
+            this.lblStudentName.AutoSize = true;
+            this.lblStudentName.Location = new System.Drawing.Point(177, 45);
+            this.lblStudentName.Name = "lblStudentName";
+            this.lblStudentName.Size = new System.Drawing.Size(72, 13);
+            this.lblStudentName.TabIndex = 17;
+            this.lblStudentName.Text = "StudentName";
+            // 
+            // lblCourseName
+            // 
+            this.lblCourseName.AutoSize = true;
+            this.lblCourseName.Location = new System.Drawing.Point(488, 45);
+            this.lblCourseName.Name = "lblCourseName";
+            this.lblCourseName.Size = new System.Drawing.Size(68, 13);
+            this.lblCourseName.TabIndex = 18;
+            this.lblCourseName.Text = "CourseName";
+            // 
+            // SchoolForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCourseName);
+            this.Controls.Add(this.lblStudentName);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.txtStudentName);
+            this.Controls.Add(this.txtCourseName);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.btnDeleteCourse);
+            this.Controls.Add(this.btnDeleteAddress);
+            this.Controls.Add(this.btnDeleteStudent);
+            this.Controls.Add(this.btnUpdateCourse);
+            this.Controls.Add(this.btnUpdateAddress);
+            this.Controls.Add(this.btnUpdateStudent);
+            this.Controls.Add(this.btnInsertCourse);
+            this.Controls.Add(this.btnInsertAddress);
+            this.Controls.Add(this.btnInsertStudent);
+            this.Controls.Add(this.btnSelectCourse);
+            this.Controls.Add(this.btnSelectAddress);
+            this.Controls.Add(this.btnSelectStudent);
+            this.Name = "SchoolForm";
+            this.Text = "SchoolForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectStudent;
+        private System.Windows.Forms.Button btnSelectAddress;
+        private System.Windows.Forms.Button btnSelectCourse;
+        private System.Windows.Forms.Button btnInsertCourse;
+        private System.Windows.Forms.Button btnInsertAddress;
+        private System.Windows.Forms.Button btnInsertStudent;
+        private System.Windows.Forms.Button btnUpdateCourse;
+        private System.Windows.Forms.Button btnUpdateAddress;
+        private System.Windows.Forms.Button btnUpdateStudent;
+        private System.Windows.Forms.Button btnDeleteCourse;
+        private System.Windows.Forms.Button btnDeleteAddress;
+        private System.Windows.Forms.Button btnDeleteStudent;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtCourseName;
+        private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblStudentName;
+        private System.Windows.Forms.Label lblCourseName;
+    }
+}
+
---
+++ Week15/ConnectingToDBapp/EntityPractice/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityPractice
+{
+    public partial class SchoolForm : Form
+    {
+        public SchoolForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectStudent_Click(object sender, EventArgs e)
+        {
+            SchoolDBEntities db = new SchoolDBEntities();
+            string studentName = "Bill";
+            string sql = "Select * from student where studentName = '" + studentName + "'";
+            List<Student> students = db.Students.SqlQuery(sql).ToList<Student>();
+
+            string output = string.Empty;
+            foreach (var student in students)
+            {
+                output = output + student.StudentName +" " + student.StandardId + Environment.NewLine;
+            }
+
+            MessageBox.Show(output);
+
+            
+            db.Dispose();
+        }
+
+
+
+        private void btnInsertStudent_Click(object sender, EventArgs e)
+        {
+            SchoolDBEntities db = new SchoolDBEntities();
+
+            // instead insert query we use this:
+            Student student = new Student();
+            student.StudentName = "Someone";
+            student.StandardId = 1;
+            db.Students.Add(student);
+
+            int output = db.SaveChanges();
+            
+            MessageBox.Show(output.ToString());
+
+            db.Dispose();
+        }
+
+        private void btnUpdateStudent_Click(object sender, EventArgs e)
+        {
+            SchoolDBEntities db = new SchoolDBEntities();
+
+            // instead update query we use this:            
+            Student student = db.Students.First<Student>();     // Ok this is first but how to find someone by name, or id?
+            student.StudentName = "Bill";
+            int output = db.SaveChanges();
+
+            MessageBox.Show(output.ToString());
+
+            db.Dispose();
+        }
+
+        private void btnDeleteStudent_Click(object sender, EventArgs e)
+        {
+            SchoolDBEntities db = new SchoolDBEntities();
+
+            // instead delete query we use this:
+            Student student = db.Students.First<Student>();     // Ok this is first but how to find someone by name, or id?
+            db.Students.Remove(student);
+            int output = db.SaveChanges();
+
+            MessageBox.Show(output.ToString());
+
+            db.Dispose();
+        }
+    }
+}
